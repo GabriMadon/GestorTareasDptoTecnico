0e2d8bb1580a91fe4f54d9af6b7f13c972240f2f
GabriMadon <deomaravilla83@gmail.com>
2025-11-26 21:39:32 -0500

    Implementacion de navegacion SPA vista Registro, vista Lista

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 document.getElementById('task-registration-form').addEventListener('submit',btnSubmit);
 
-
+// Funcio Formulario
 function btnSubmit(event){
     event.preventDefault()//Detiene el envio del formulario
 
@@ -13,7 +13,7 @@ function btnSubmit(event){
     const equipmentType = document.getElementById('equipment-type').value;
     const equipmentMarca = document.getElementById('equipment-marca').value;
     const equipmentModelo = document.getElementById('equipment-modelo').value;
-    const equipmentAnio = document.getElementById('equipment-anio').value;
+    // const equipmentAnio = document.getElementById('equipment-anio').value;
     const equipmentSerie = document.getElementById('equipment-serie').value;
     const equipmentDescription = document.getElementById('problem-description').value;
     const equipmentPriority = document.getElementById('priority').value;
@@ -34,7 +34,7 @@ function btnSubmit(event){
         tipoEquipo: equipmentType,
         marca: equipmentMarca,
         modelo:equipmentModelo,
-        anio: equipmentAnio,
+        // anio: equipmentAnio,
         serie: equipmentSerie,
         descripcion: equipmentDescription,
         prioridad:equipmentPriority,
@@ -53,6 +53,7 @@ function btnSubmit(event){
 
 }
 
+//Funcion showToast
 function showToast(message, type = "success") {
   const toast = document.getElementById("toast");
   toast.textContent = message;
@@ -68,4 +69,48 @@ function showToast(message, type = "success") {
     toast.classList.remove("show");
   }, 3000);
 }
+
+//Funcion Vie Registro-Lista SPA
+
+function showView(viewName){
+  //id del DOM en variables
+  const registrationView = document.getElementById('registration-view')
+  const listView = document.getElementById('list-view')
+  const toggleBtn = document.getElementById('toggle-view-btn')
+  const titleText = document.getElementById('title-text')
+
+  if(viewName==='registration'){
+    registrationView.style.display='block';
+    listView.style.display='none';
+    toggleBtn.textContent='Ver Lista de Equipos';
+    titleText.textContent='Registrar Equipo';
+    currentView = 'registration';
+    console.log("Vista Registro")
+
+  }else if(viewName==='list'){
+    registrationView.style.display='none';
+    listView.style.display='block';
+    toggleBtn.textContent='Ver Formulario';
+    titleText.textContent='Equipos Registrados';
+    currentView = 'list';
+    console.log("Vista Lista")
+  }
+
+  console.log("btn toggle en funcionamiento")
+
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const toggleBtn = document.getElementById('toggle-view-btn')
+
+  if(toggleBtn){
+    toggleBtn.addEventListener('click', ()=>{
+      const nextView = currentView === 'registration' ? 'list' : 'registration';
+      showView(nextView)
+    })
+
+  }
+  showView('registration')
+})
+
 
